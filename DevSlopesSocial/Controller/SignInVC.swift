baed7009d9bddd56437d951d2e27ccd00d9b2a1a
Stevan Medic <stevan.medic@outlook.com>
2018-06-07 17:49:19 +0200

    Initial commit - pods and firebase

---
+++ DevSlopesSocial/Controller/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DevSlopesSocial
+//
+//  Created by Stevan Medic on 6/7/18.
+//  Copyright © 2018 Stevan Medic. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-DevSlopesSocial/Pods-DevSlopesSocial-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DevSlopesSocial : NSObject
+@end
+@implementation PodsDummy_Pods_DevSlopesSocial
+@end
